af9a2e55f242aeee01a8cdadf4e6b203a8a295e5
Alex Gaynor <alex.gaynor@rd.io>
2013-02-23 10:37:22 -0800

    a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name="json_writer",
+    version="0.1",
+    description="An iterative JSON writer",
+    author="Alex Gaynor",
+    author_email="alex.gaynor@gmail.com",
+    url="https://github.com/alex/json_writer",
+    packages=["json_writer"],
+)
